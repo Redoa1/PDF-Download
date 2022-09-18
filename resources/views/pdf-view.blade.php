99f1181911c7b9ac7e6eb8a59d372856ac09614a
Redoan Ahmed <redoanredoan23@gmail.com>
2022-09-18 15:07:35 +0600

    first commit

---
+++ resources/views/pdf-view.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Created At</th>   
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>{{ $user['id'] }}</td>
+        <td>{{ $user['name'] }}</td>
+        <td>{{ $user['email'] }}</td>
+        <td>{{ $user['created_at'] }}</td>
+      </tr>
+      </tbody>
+
+    </table>
+  </body>
+</html>
